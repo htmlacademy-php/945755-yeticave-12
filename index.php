1a9c0d5cdf4a884232a7efce7c57ba9b954d67b9
Кирилл <kirill-vologda@yandex.ru>
2021-11-02 15:15:28 +0300

    Работа с условием

--- index.php
+++ index.php
@@ -1,37 +1,50 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = ''; // укажите здесь ваше имя
+$user_name = 'Кирилл';
 ?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <title>Главная</title>
-    <link href="../css/normalize.min.css" rel="stylesheet">
-    <link href="../css/style.css" rel="stylesheet">
+<meta charset="UTF-8">
+<title>Главная</title>
+<link href="../css/normalize.min.css" rel="stylesheet">
+<link href="../css/style.css" rel="stylesheet">
 </head>
 <body>
 <div class="page-wrapper">
 
 <header class="main-header">
-    <div class="main-header__container container">
-        <h1 class="visually-hidden">YetiCave</h1>
-        <a class="main-header__logo">
-            <img src="../img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
-        </a>
-        <form class="main-header__search" method="get" action="https://echo.htmlacademy.ru" autocomplete="off">
-            <input type="search" name="search" placeholder="Поиск лота">
-            <input class="main-header__search-btn" type="submit" name="find" value="Найти">
-        </form>
-        <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
+<div class="main-header__container container">
+<h1 class="visually-hidden">YetiCave</h1>
+<a class="main-header__logo">
+<img src="../img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
+</a>
+<form class="main-header__search" method="get" action="https://echo.htmlacademy.ru" autocomplete="off">
+<input type="search" name="search" placeholder="Поиск лота">
+<input class="main-header__search-btn" type="submit" name="find" value="Найти">
+</form>
+<a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
-        <nav class="user-menu">
-
-        <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-
-        </nav>
-    </div>
+<nav class="user-menu">
+<?php if ($is_auth == 1): ?>
+<div class="user-menu__logged">
+<p><?php print($user_name) ?></p>
+<a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+<a class="user-menu__logout" href="#">Выход</a>
+</div>
+<?php else: ?>
+<ul class="user-menu__list">
+<li class="user-menu__item">
+<a href="#">Регистрация</a>
+</li>
+<li class="user-menu__item">
+<a href="#">Вход</a>
+</li>
+</ul>
+<?php endif; ?>
+</nav>
+</div>
 </header>
 
 <main class="container">
