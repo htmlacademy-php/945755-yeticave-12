ff9f3ac061468c91958d57838ed1574a01ff75b1
Кирилл <kirill-vologda@yandex.ru>
2021-11-10 20:26:19 +0300

    Вроде исправил все замечания

--- index.php
+++ index.php
@@ -13,37 +13,37 @@ $products = [
         'title' => '2014 Rossignol District Snowboard',
         'category' => $categories['boards'],
         'price' => 10999,
-        'Url_picture' => 'img/lot-1.jpg'
+        'url_picture' => 'img/lot-1.jpg'
     ],
     [
         'title' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => $categories['boards'],
         'price' => 159999,
-        'Url_picture' => 'img/lot-2.jpg'
+        'url_picture' => 'img/lot-2.jpg'
     ],
     [
         'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => $categories['fasteners'],
         'price' => 8000,
-        'Url_picture' => 'img/lot-3.jpg'
+        'url_picture' => 'img/lot-3.jpg'
     ],
     [
         'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => $categories['boots'],
         'price' => 10999,
-        'Url_picture' => 'img/lot-4.jpg'
+        'url_picture' => 'img/lot-4.jpg'
     ],
     [
         'title' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => $categories['clothes'],
         'price' => 7500,
-        'Url_picture' => 'img/lot-5.jpg'
+        'url_picture' => 'img/lot-5.jpg'
     ],
     [
         'title' => 'Маска Oakley Canopy',
         'category' => $categories['other'],
         'price' => 5400,
-        'Url_picture' => 'img/lot-6.jpg'
+        'url_picture' => 'img/lot-6.jpg'
     ]
 ];
 ?>
@@ -99,8 +99,8 @@ $products = [
                 <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
                 <ul class="promo__list">
                     <?php foreach ($categories as $modifier => $category) : ?>
-                        <li class="promo__item promo__item--boards">
-                            <a class="promo__link" href="pages/all-lots.html"><?= $categories[$modifier]; ?></a>
+                        <li class="promo__item promo__item--<?= $modifier ?>">
+                            <a class="promo__link" href="pages/all-lots.html"><?= $category ?></a>
                         </li>
                     <?php endforeach; ?>
                 </ul>
@@ -113,14 +113,14 @@ $products = [
                     <?php foreach ($products as $product) : ?>
                         <li class="lots__item lot">
                             <div class="lot__image">
-                                <img src="<?= $product['Url_picture'] ?>" width="350" height="260" alt="">
+                                <img src="<?= $product['url_picture'] ?>" width="350" height="260" alt="">
                             </div>
                             <div class="lot__info">
                                 <span class="lot__category"><?= $product['category'] ?></span>
                                 <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $product['title'] ?></a></h3>
                                 <div class="lot__state">
                                     <div class="lot__rate">
-                                        <span class="lot__amount"><?= $product['price'] ?></span>
+                                        <span class="lot__amount">Стартовая цена</span>
                                         <span class="lot__cost"><?= $product['price'] ?><b class="rub">р</b></span>
                                     </div>
                                     <div class="lot__timer timer">
@@ -138,11 +138,10 @@ $products = [
     <footer class="main-footer">
         <nav class="nav">
             <ul class="nav__list container">
-                <?php foreach ($categories as $modifier => $category) : ?>
+                <?php foreach ($categories as $category) : ?>
                     <li class="nav__item">
-                        <a href="pages/all-lots.html"><?= $categories[$modifier]; ?></a>
+                        <a href="pages/all-lots.html"><?= $category ?></a>
                     </li>
-                    <?php $footerindex++; ?>
                 <?php endforeach; ?>
 
             </ul>
