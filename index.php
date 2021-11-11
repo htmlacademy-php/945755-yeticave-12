01bb62156ee25075775deaa426f6bfa53124752f
Кирилл <kirill-vologda@yandex.ru>
2021-11-11 23:01:03 +0300

    Добавил функцию

--- index.php
+++ index.php
@@ -46,6 +46,17 @@ $products = [
         'url_picture' => 'img/lot-6.jpg'
     ]
 ];
+
+function formatting($num) {
+    if ($num > 999) {
+        $num = number_format(ceil($num), 0, '', ' ',);
+        print($num . ' ' . '₽');
+    }
+    else {
+        print(ceil($num) . ' ' . '₽');
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -121,7 +132,7 @@ $products = [
                                 <div class="lot__state">
                                     <div class="lot__rate">
                                         <span class="lot__amount">Стартовая цена</span>
-                                        <span class="lot__cost"><?= $product['price'] ?><b class="rub">р</b></span>
+                                        <span class="lot__cost"><?= formatting($product['price']) ?></span>
                                     </div>
                                     <div class="lot__timer timer">
                                         12:23
